ca05eef6ade071af11c0347fe054b92e7d623ed9
Styrfire <g_n_rogers1991@yahoo.com>
2018-01-10 21:21:31 -0600

    Moved to using Spring Boot's RestTemplate for making the calls as opposed to doing it manually

--- src/main/groovy/com/riot/api/QueryManager.java
+++ src/main/groovy/com/riot/api/QueryManager.java
@@ -3,11 +3,9 @@ package com.riot.api;
 import com.riot.enums.METHOD;
 import com.riot.exception.RiotApiException;
 import com.riot.exception.RiotExceptionCreator;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -15,11 +13,13 @@ class QueryManager
 {
 	private final String apiKey;
 	private final RiotRateLimiter rateLimiter;
+	private final RestTemplate restTemplate;
 
 	QueryManager(String apiKey)
 	{
 		this.apiKey = apiKey;
 		this.rateLimiter = new RiotRateLimiter();
+		this.restTemplate = new RestTemplate();
 	}
 
 	String query(String queryUrl, METHOD method) throws RiotApiException
@@ -32,17 +32,14 @@ class QueryManager
 			// make the api call
 			String urlString = "https://na1.api.riotgames.com" + queryUrl + "api_key=" + apiKey;
 			System.out.println("urlString = " + urlString);
-			URL url = new URL(urlString);
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod("GET");
-			connection.setDoOutput(true);
+			ResponseEntity<String> responseEntity = restTemplate.getForEntity(urlString, String.class);
 
 			// handle exceptions
-			if (connection.getResponseCode() >= 300)
-				RiotExceptionCreator.throwException(connection.getResponseCode());
+			if (responseEntity.getStatusCode().value() >= 300)
+				RiotExceptionCreator.throwException(responseEntity.getStatusCode().value());
 
 			// rate limit headers
-			Map<String, List<String>> headers = connection.getHeaderFields();
+			Map<String, List<String>> headers = responseEntity.getHeaders();
 			System.out.println(headers.get("X-App-Rate-Limit"));
 			System.out.println(headers.get("X-App-Rate-Limit-Count"));
 			System.out.println(headers.get("X-Method-Rate-Limit"));
@@ -51,18 +48,8 @@ class QueryManager
 			// updated method rate limit objects
 			rateLimiter.postApiCallRateLimit(method, headers);
 
-			// Get the response
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			String line;
-			StringBuilder response = new StringBuilder();
-			while ((line = bufferedReader.readLine()) != null)
-			{
-				response.append(line);
-			}
-			bufferedReader.close();
-			System.out.println("ResponseCode = " + connection.getResponseCode());
-
-			return response.toString();
+			// return the response
+			return responseEntity.getBody();
 		}
 		catch (RiotApiException e)
 		{
