c0f8447d6421ade228c66775abc70d421e6b8329
Styrfire <g_n_rogers1991@yahoo.com>
2018-01-10 21:07:35 -0600

    Abstracted rate limiting code into it's own class

--- src/main/groovy/com/riot/api/QueryManager.java
+++ src/main/groovy/com/riot/api/QueryManager.java
@@ -1,7 +1,5 @@
 package com.riot.api;
 
-import com.google.common.util.concurrent.RateLimiter;
-import com.riot.dto.RateLimiter.RateLimiterListData;
 import com.riot.enums.METHOD;
 import com.riot.exception.RiotApiException;
 import com.riot.exception.RiotExceptionCreator;
@@ -10,123 +8,26 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class QueryManager
 {
 	private final String apiKey;
+	private final RiotRateLimiter rateLimiter;
 
 	QueryManager(String apiKey)
 	{
 		this.apiKey = apiKey;
+		this.rateLimiter = new RiotRateLimiter();
 	}
 
 	String query(String queryUrl, METHOD method) throws RiotApiException
 	{
-		Map<METHOD, RateLimiterListData> rateLimiterList = new HashMap<>();
  		try
 		{
-			boolean methodInList = false;
-			// is method on rateLimiterList
-			if (rateLimiterList.containsKey(method))
-			{
-				methodInList = true;
-			}
-
-			// if method is on rateLimiterList
-			if (methodInList)
-			{
-				// if method is outside time window, remove it from list because the time window has reset
-				if (rateLimiterList.get(method).getRateLimiter().tryAcquire())
-				{
-					rateLimiterList.remove(method);
-					methodInList = false;
-				}
-				// if method is still inside time window
-				else
-				{
-					// if method doesn't have anymore calls left within time window, wait, then remove from the list
-					// because time window has reset
-					if (rateLimiterList.get(method).getNumOfCalls() >= rateLimiterList.get(method).getMaxNumOfCalls())
-					{
-						rateLimiterList.get(method).getRateLimiter().acquire();
-						rateLimiterList.remove(method);
-						methodInList = false;
-					}
-				}
-			}
-
-			boolean appShortInList = false;
-			// is app on rateLimiterList
-			if (rateLimiterList.containsKey(METHOD.API_SHORT))
-			{
-				appShortInList = true;
-			}
-
-			// if app is on rateLimiterList
-			if (appShortInList)
-			{
-				// if app is outside time window, remove it from list because the time window has reset
-				if (rateLimiterList.get(METHOD.API_SHORT).getRateLimiter().tryAcquire())
-				{
-					rateLimiterList.remove(METHOD.API_SHORT);
-					appShortInList = false;
-				}
-				// if method is still inside time window
-				else
-				{
-					// if method doesn't have anymore calls left within time window, wait, then remove from the list
-					// because time window has reset
-					if (rateLimiterList.get(METHOD.API_SHORT).getNumOfCalls() >= rateLimiterList.get(METHOD.API_SHORT).getMaxNumOfCalls())
-					{
-						rateLimiterList.get(METHOD.API_SHORT).getRateLimiter().acquire();
-						rateLimiterList.remove(METHOD.API_SHORT);
-						appShortInList = false;
-					}
-				}
-			}
-
-			boolean appLongInList = false;
-			// is app on rateLimiterList
-			if (rateLimiterList.containsKey(METHOD.API_LONG))
-			{
-				appLongInList = true;
-			}
-
-			// if app is on rateLimiterList
-			if (appLongInList)
-			{
-				// if app is outside time window, remove it from list because the time window has reset
-				if (rateLimiterList.get(METHOD.API_LONG).getRateLimiter().tryAcquire())
-				{
-					rateLimiterList.remove(METHOD.API_LONG);
-					appLongInList = false;
-				}
-				// if method is still inside time window
-				else
-				{
-					// if method doesn't have anymore calls left within time window, wait, then remove from the list
-					// because time window has reset
-					if (rateLimiterList.get(METHOD.API_LONG).getNumOfCalls() >= rateLimiterList.get(METHOD.API_LONG).getMaxNumOfCalls())
-					{
-						rateLimiterList.get(METHOD.API_LONG).getRateLimiter().acquire();
-						rateLimiterList.remove(METHOD.API_LONG);
-						appLongInList = false;
-					}
-				}
-			}
-
-			//respect rate limit
-//			if (!shortRateLimiter.tryAcquire())
-//				System.out.println("ShortRateLimiterFailed");
-//			if (!longRateLimiter.tryAcquire())
-//				System.out.println("LongRateLimiterFailed");
-//
-//			shortRateLimiter.acquire();
-//			longRateLimiter.acquire();
+			// check to make sure the method isn't exceeding it's rate limit
+			rateLimiter.preApiCallRateLimit(method);
 
 			// make the api call
 			String urlString = "https://na1.api.riotgames.com" + queryUrl + "api_key=" + apiKey;
@@ -147,78 +48,8 @@ class QueryManager
 			System.out.println(headers.get("X-Method-Rate-Limit"));
 			System.out.println(headers.get("X-Method-Rate-Limit-Count"));
 
-			// if method wasn't on rateLimiterList, add it
-			if (!methodInList)
-			{
-				Double timeWindow = Double.parseDouble(Arrays.asList(headers.get("X-Method-Rate-Limit").get(0).split(":")).get(1));
-
-				RateLimiterListData rateLimiterListData = new RateLimiterListData();
-				rateLimiterListData.setRateLimiter(RateLimiter.create(1/timeWindow));
-				rateLimiterListData.setNumOfCalls(Integer.parseInt(Arrays.asList(headers.get("X-Method-Rate-Limit-Count").get(0).split(":")).get(0)));
-				rateLimiterListData.setMaxNumOfCalls(Integer.parseInt(Arrays.asList(headers.get("X-Method-Rate-Limit").get(0).split(":")).get(0)));
-				rateLimiterListData.setTimeWindowInSeconds(Integer.parseInt(Arrays.asList(headers.get("X-Method-Rate-Limit-Count").get(0).split(":")).get(1)));
-
-				System.out.println("methodTimeWindow = " + timeWindow +
-						"\nmethodNumOfCalls = " + rateLimiterListData.getNumOfCalls() +
-						"\nmethodMaxNumOfCalls = " + rateLimiterListData.getMaxNumOfCalls() +
-						"\nmethodTimeWindowInSeconds = " + rateLimiterListData.getTimeWindowInSeconds());
-
-				rateLimiterList.put(method, rateLimiterListData);
-			}
-			// if method was on list, update the number of calls it's made within the time window
-			else
-			{
-				rateLimiterList.get(method).setNumOfCalls(rateLimiterList.get(method).getNumOfCalls() + 1);
-			}
-
-			// static methods don't count against the application api and won't have the headers
-			// if api short wasn't on rateLimiterList and the method wasn't static, add it
-			if (!appShortInList && (method != METHOD.STATIC))
-			{
-				Double timeWindow = Double.parseDouble(Arrays.asList(headers.get("X-App-Rate-Limit").get(0).split("[:,]")).get(1));
-
-				RateLimiterListData rateLimiterListData = new RateLimiterListData();
-				rateLimiterListData.setRateLimiter(RateLimiter.create(1/timeWindow));
-				rateLimiterListData.setNumOfCalls(Integer.parseInt(Arrays.asList(headers.get("X-App-Rate-Limit-Count").get(0).split("[:,]")).get(0)));
-				rateLimiterListData.setMaxNumOfCalls(Integer.parseInt(Arrays.asList(headers.get("X-App-Rate-Limit").get(0).split("[:,]")).get(0)));
-				rateLimiterListData.setTimeWindowInSeconds(Integer.parseInt(Arrays.asList(headers.get("X-App-Rate-Limit-Count").get(0).split("[:,]")).get(1)));
-
-				System.out.println("appShortTimeWindow = " + timeWindow +
-				"\nappShortNumOfCalls = " + rateLimiterListData.getNumOfCalls() +
-				"\nappShortMaxNumOfCalls = " + rateLimiterListData.getMaxNumOfCalls() +
-				"\nappShortTimeWindowInSeconds = " + rateLimiterListData.getTimeWindowInSeconds());
-
-				rateLimiterList.put(METHOD.API_SHORT, rateLimiterListData);
-			}
-			// if api short was on list and the method wasn't static, update the number of calls it's made within the time window
-			else if (method != METHOD.STATIC)
-			{
-				rateLimiterList.get(METHOD.API_SHORT).setNumOfCalls(rateLimiterList.get(METHOD.API_SHORT).getNumOfCalls() + 1);
-			}
-
-			// if api long wasn't on rateLimiterList and the method wasn't static, add it
-			if (!appLongInList && (method != METHOD.STATIC))
-			{
-				Double timeWindow = Double.parseDouble(Arrays.asList(headers.get("X-App-Rate-Limit").get(0).split("[:,]")).get(3));
-
-				RateLimiterListData rateLimiterListData = new RateLimiterListData();
-				rateLimiterListData.setRateLimiter(RateLimiter.create(1/timeWindow));
-				rateLimiterListData.setNumOfCalls(Integer.parseInt(Arrays.asList(headers.get("X-App-Rate-Limit-Count").get(0).split("[:,]")).get(2)));
-				rateLimiterListData.setMaxNumOfCalls(Integer.parseInt(Arrays.asList(headers.get("X-App-Rate-Limit").get(0).split("[:,]")).get(2)));
-				rateLimiterListData.setTimeWindowInSeconds(Integer.parseInt(Arrays.asList(headers.get("X-App-Rate-Limit-Count").get(0).split("[:,]")).get(3)));
-
-				System.out.println("appLongTimeWindow = " + timeWindow +
-				"\nappLongNumOfCalls = " + rateLimiterListData.getNumOfCalls() +
-				"\nappLongMaxNumOfCalls = " + rateLimiterListData.getMaxNumOfCalls() +
-				"\nappLongTimeWindowInSeconds = " + rateLimiterListData.getTimeWindowInSeconds());
-
-				rateLimiterList.put(METHOD.API_LONG, rateLimiterListData);
-			}
-			// if api long was on list and the method wasn't static, update the number of calls it's made within the time window
-			else if (method != METHOD.STATIC)
-			{
-				rateLimiterList.get(METHOD.API_LONG).setNumOfCalls(rateLimiterList.get(METHOD.API_LONG).getNumOfCalls() + 1);
-			}
+			// updated method rate limit objects
+			rateLimiter.postApiCallRateLimit(method, headers);
 
 			// Get the response
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
